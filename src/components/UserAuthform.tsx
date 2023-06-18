42aa0622d85c2b08ab1f5fa43f61adaf5818f8ee
Milan Lakhani <milanlakhaniaiml@gmail.com>
2023-06-18 15:17:40 -0400

    feat: add toast from shadcn

--- src/components/UserAuthform.tsx
+++ src/components/UserAuthform.tsx
@@ -1,6 +1,10 @@
-import { FC } from 'react'
+"use client"
+
+import { FC, useState } from 'react'
 import { Button } from './ui/Button'
 import { cn } from '@/lib/utils'
+import { signIn } from 'next-auth/react'
+import { Icons } from './Icons'
 
 interface UserAuthformProps extends React.HTMLAttributes<HTMLDivElement> {
 
@@ -8,10 +12,24 @@ interface UserAuthformProps extends React.HTMLAttributes<HTMLDivElement> {
 
 const UserAuthform: FC<UserAuthformProps> = ({ className, ...props }) => {
     
-    
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const loginWithGoogle = async () => {
+
+        try {
+            setIsLoading(true)
+
+            await signIn('google')
+        } catch (error) {
+            // Toast error
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
     return <div className={cn(className, 'flex justify-center')} {...props}>
-        <Button size='sm' className='w-full'>
+        <Button onClick={loginWithGoogle} isLoading={isLoading} size='sm' className='w-full'>
+            {isLoading ? null : <Icons.google className='h-4 w-4 mr-2' />}
             Sign In with Google
         </Button>
     </div>
