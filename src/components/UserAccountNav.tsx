6740cddcccb828507a8dde37df57f17d19e3048b
Milan Lakhani <milanlakhaniaiml@gmail.com>
2023-06-19 15:11:22 -0400

    chore: navbar profile menu and sign-out

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,7 +1,10 @@
+"use client"
 import { User } from "next-auth";
 import { FC } from "react";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/DropdownMenu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/DropdownMenu";
 import UserAvatar from "./UserAvatar";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
 
 interface UserAccountNavProps {
     user: Pick<User, 'name' | 'email' | 'image'>
@@ -12,12 +15,48 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
         <DropdownMenuTrigger>
 
             <UserAvatar
-             className="h-8 w-8"
-             user={{
-                name: user.name || null,
-                image: user.image || null
-            }} />
+                className="h-8 w-8"
+                user={{
+                    name: user.name || null,
+                    image: user.image || null
+                }} />
         </DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-white" align="end">
+            <div className="flex items-center justify-start gap-2 p-2">
+                <div className="flex flex-col space-y-1 leading-none">
+                    {user?.name && <p className="font-medium">{user.name}</p>}
+                    {user?.email && <p className="w-[200px] truncate text-sm text-zinc-700">{user.email}</p>}
+                </div>
+            </div>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem asChild>
+                <Link href="/">Feed</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+                <Link href="/r/create">Create a new Community</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+                <Link href="/settings">Settings</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem asChild>
+                <Link href="/">Feed</Link>
+            </DropdownMenuItem>
+
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem className="cursor-pointer text-red-500" onSelect={(e) => {
+                e.preventDefault()
+                signOut({
+                    callbackUrl: `${window.location.origin}/sign-in`
+                })
+            }}>
+                Sign Out
+            </DropdownMenuItem>
+
+        </DropdownMenuContent >
+
     </DropdownMenu>
 }
 
