fe09ae1fd7bc53ab90ec3fa48e8ed3ebd1950707
Milan Lakhani <milanlakhaniaiml@gmail.com>
2023-06-24 17:04:47 -0400

    fix: post not showing, feed data config

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -176,7 +176,7 @@ const Editor: FC<EditorProps> = ({ subredditId }) => {
     const { ref: titleRef, ...rest } = register('title')
 
     return <div className='w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200'>
-        <form id='subreddit-post-form' className='w-fit' onSubmit={handleSubmit(() => { })}>
+        <form id='subreddit-post-form' className='w-fit' onSubmit={handleSubmit(onSubmit)}>
             <div className='prose prose-stone dark:prose-invert'>
                 <TextareaAutosize
                     ref={(e) => {
---
+++ src/components/Post.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface PostProps {
+  
+}
+
+const Post: FC<PostProps> = ({}) => {
+  return <div>Posts</div>
+}
+
+export default Post
